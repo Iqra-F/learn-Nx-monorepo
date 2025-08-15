b53073dea0684a32344bf5e263a87e8d98a28c60
Fatima_Iqra <iqraf683@gmail.com>
2025-08-15 17:56:42 +0500

    Move dashboard app to /apps, fix tsconfig and Cypress config, all tests passing

--- apps/my-space-e2e/cypress.config.ts
+++ apps/my-space-e2e/cypress.config.ts
@@ -1,4 +1,4 @@
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset.js';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
